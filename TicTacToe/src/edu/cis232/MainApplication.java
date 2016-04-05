9610db1e8cb735ce88e20523e90efa974880ebd0
SK207_12 <SK207_12@K207_W812.carrollcc.edu>
2016-04-04 20:11:18 -0400

    Update

---
+++ TicTacToe/src/edu/cis232/MainApplication.java
@@ -0,0 +1,30 @@
+package edu.cis232;
+
+import java.io.FileInputStream;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MainApplication extends Application {
+	
+		public static void main(String[] args){
+			launch(args);
+		}
+
+		@Override
+		public void start(Stage stage) throws Exception {
+			FXMLLoader loader = new FXMLLoader();
+			Parent parent = loader.load(new FileInputStream("game.fxml"));
+
+			Scene scene = new Scene(parent);
+
+			stage.setTitle("Tic Tac Toe");
+			stage.setScene(scene);
+			stage.show();
+			
+		}
+
+}
